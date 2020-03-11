ab0790f9990cc6b916ef13a35745c91aa98f334d
Jeff Cho <jeffcho0227@gmail.com>
2020-03-10 23:32:18 -0700

    feat: added funcitonality for ask button to change the ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-    @IBOutlet var butllImageView: [UIImageView]!
+    let ballArray = [#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball5")]
     
-
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     @IBAction func ask(_ sender: Any) {
-        print("ask")
+        ballImageView.image = ballArray[Int.random(in: 0...4)]
     }
 }
 
